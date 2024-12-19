0c8ada5537cf258e8a9ff8e7c7d6bc896b3f508b
incid <incid@DESKTOP-LFA5FLK>
2024-12-19 11:47:48 +0300

    Proje dosyası ekle.

---
+++ ali, ali ali  , ali ali ali/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ali__ali_ali____ali_ali_ali
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=3; i++)
+            {
+
+
+
+                string isim =  " ali";
+                for (int j = 1; j <= i; j++)
+                {
+                    isim +=   "ali";
+
+                }
+                Console.WriteLine(   isim);
+                    }
+        }
+    }
+}
